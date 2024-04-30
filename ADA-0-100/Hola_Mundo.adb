00a8baf2b742f190710564c83290d8dcfc0dd7c5
davidbachii <david.bachiller@edu.uah.es>
2024-04-30 17:04:17 +0200

    Compilador para visual

---
+++ ADA-0-100/Hola_Mundo.adb
@@ -0,0 +1,6 @@
+with Ada . Text_IO ; -- Incluimos el paquete Text_IO de Ada
+use Ada . Text_IO ; -- Es similar a namespace en C ++
+procedure Hola_Mundo is
+begin
+Put_Line ( " Hola mundo ! " ) ; -- Put_Line esta en Text_IO
+end Hola_Mundo ;
