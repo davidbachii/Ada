30cb36124c43c13532d1bdf666eabbd5f1c6708f
davidbachii <david.bachiller@edu.uah.es>
2024-05-04 13:04:46 +0200

    P1 terminada

--- ADA-0-100/registros.adb
+++ ADA-0-100/registros.adb
@@ -18,14 +18,6 @@ use meses_IO;
 package anios_IO is new Ada.Text_IO.Integer_IO(anios_t);
 use anios_IO;
 
--- Definimos el tipo de dato fecha_t que es un registro con los campos dia, mes y anio
-
-type fecha_t is record
-  dia : dias_t;
-  mes : meses_t;
-  anio : anios_t;
-end record;
-
 -- TEMPERATURA
 
 type temperatura_t is delta 0.01 range -25.00 .. 75.00; -- Definimos el tipo de dato temperatura_t que va de -25 a 75 de tipo real
@@ -35,28 +27,47 @@ type temperatura_t is delta 0.01 range -25.00 .. 75.00; -- Definimos el tipo de 
 package temperatura_IO is new Ada.Text_IO.Fixed_IO(temperatura_t);
 use temperatura_IO;
 
+-- Definimos el tipo de dato fecha_t que es un registro con los campos dia, mes y anio
+
+type fecha_t is record
+  dia : dias_t;
+  mes : meses_t;
+  anio : anios_t;
+  temperatura : temperatura_t;
+end record;
+
+
+-- Aqui tenemos dos opciones y es crear un array y ya pasarle un tamaño fijo
 
 type array_fechas is array(integer range <>) of fecha_t; -- Definimos el tipo de dato array_fechas que es un array de fechas
-type array_temperaturas is array(integer range <>) of temperatura_t; -- Definimos el tipo de dato array_temperaturas que es un array de temperaturas
 
 
-array_f : array_fechas(1 .. 10); -- Declaramos un array de fechas de 10 elementos
-array_t : array_temperaturas(1 .. 10); -- Declaramos un array de temperaturas de 10 elementos
+-- El problema que nosotros pedimos por pantalla el numero de elementos que va a recoger el array
+-- array_f : array_fechas(1 .. 10); -- Declaramos un array de fechas de 10 elementos
 
+
+-- La mejor opcion es crear un puntero al ese array por que los punteros nos permiten cambiar sus valores
+
+
+type ptr_fechas is access array_fechas;
+
+array_f : ptr_fechas := new array_fechas(1 .. 10); --Creamos un puntero al array de fechas que en un futuro modificaremos
+
+--  Numero de elementos 
 package Integer_IO is new Ada.Text_IO.Integer_IO(integer);
 use Integer_IO;
 
 numero_registros : integer; -- Declaramos una variable de tipo entero numero_registros
 
-function maximo (array_TEM : array_temperaturas) return temperatura_t is
-  max : temperatura_t := array_TEM(array_TEM'First);
+function maximo (array_TEM : ptr_fechas) return temperatura_t is
+  max : temperatura_t := array_TEM(array_TEM'First).temperatura;
 
 begin
 
   for i in array_TEM'range loop
 
-    if array_TEM(i) > max then
-      max := array_TEM(i);
+    if array_TEM(i).temperatura > max then
+      max := array_TEM(i).temperatura;
     end if;
 
   end loop;
@@ -65,15 +76,15 @@ return max;
 
 end maximo;
 
-function minimo (array_TEM : array_temperaturas) return temperatura_t is
-  min : temperatura_t := array_TEM(array_TEM'First);
+function minimo (array_TEM : ptr_fechas) return temperatura_t is
+  min : temperatura_t := array_TEM(array_TEM'First).temperatura;
 
 begin
 
     for i in array_TEM'range loop
   
-        if array_TEM(i) < min then
-          min := array_TEM(i);
+        if array_TEM(i).temperatura < min then
+          min := array_TEM(i).temperatura;
         end if;
   
     end loop;
@@ -82,7 +93,7 @@ begin
   
 end minimo;
 
-function media (array_TEM : array_temperaturas) return temperatura_t is
+function media (array_TEM : ptr_fechas) return temperatura_t is
   suma : temperatura_t := 0.00;
   media : temperatura_t := 0.00;
 
@@ -90,7 +101,7 @@ begin
 
     for i in array_TEM'range loop
   
-        suma := suma + array_TEM(i);
+        suma := suma + array_TEM(i).temperatura;
   
     end loop;
 
@@ -109,12 +120,16 @@ put("Introduce el numero de registros que quieres introducir: ");
 get(numero_registros);
 
 
+-- Esto lo podemos hacer gracias a los punteros con los tipos no podemos modificar sus fatos
+array_f := new array_fechas(1 .. numero_registros); 
+
+
 -- Le pedimos al usuario que introduzca los datos de los registros
 
 for i in 1 .. numero_registros loop
 
 put("Introduce la " & i'image & " temperatura: ");
-get(array_t(i));
+get(array_f(i).temperatura);
 
 put("Introduce el dia: ");
 get(array_f(i).dia);
@@ -129,11 +144,11 @@ end loop;
 
 
 
-Put_Line("La temperatura maxima es: " & maximo(array_t)'Image);
+Put_Line("La temperatura maxima es: " & maximo(array_f)'Image);
 
-Put_Line("La temperatura minima es: " & minimo(array_t)'Image);
+Put_Line("La temperatura minima es: " & minimo(array_f)'Image);
 
-Put_Line("La temperatura media es: " & media(array_t)'Image & " alcanzada entre el " & array_f(array_t'First).dia'Image & " de " & array_f(array_t'First).mes'Image & " de " & array_f(array_t'First).anio'Image & " y el " & array_f(array_t'Last).dia'Image & " de " & array_f(array_t'Last).mes'Image & " de " & array_f(array_t'Last).anio'Image);
+Put_Line("La temperatura media es: " & media(array_f)'Image & " alcanzada entre el " & array_f(array_f'First).dia'Image & " de " & array_f(array_f'First).mes'Image & " de " & array_f(array_f'First).anio'Image & " y el " & array_f(array_f'Last).dia'Image & " de " & array_f(array_f'Last).mes'Image & " de " & array_f(array_f'Last).anio'Image);
 
 
 
@@ -143,7 +158,3 @@ Put_Line("La temperatura media es: " & media(array_t)'Image & " alcanzada entre 
 
 
 end registros;
-
-
--- Ya esta todo implementado, el problema es el tamaño del array entonces la clave va a ser crear un putero al array y ahi se que podremos tocar el array como mas nos guste, otra cosa tambien es meter
--- en el registro de fecha tambien la temperatura. Asi nos dejamos de rollos, pero se podria hacer con el POS.
